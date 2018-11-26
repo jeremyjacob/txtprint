4299c7383be8b7b651dd03bc820248aa49136c8a
jeremyjacob <44799944+jeremyjacob@users.noreply.github.com>
2018-11-26 00:56:24 -0800

    Added type on anim

--- docs/scripts.js
+++ docs/scripts.js
@@ -15,10 +15,11 @@ var observe, page;
 
 //// Load cookie ////
 if (document.cookie.split('=')[1] != null) {
-  document.addEventListener('DOMContentLoaded', event => {
-    box.value = document.cookie.split('=')[1];
-  });
-}
+  window.next_letter = function() {
+      if (index <= storedBox.length) {
+          box.value = storedBox.substr(0, index++);
+          setTimeout("next_letter()", 8);
+  }};}
 
 //// Mobile code ////
 if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
