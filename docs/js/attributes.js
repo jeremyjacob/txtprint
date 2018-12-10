a15bbcaaf8549159c44b184780c6b3d1dc420af6
Jeremy Jacob <jeremjacob5705@student.lvusd.org>
2018-12-09 21:57:46 -0800

    1.8

--- docs/js/attributes.js
+++ docs/js/attributes.js
@@ -1,8 +1,12 @@
 /*eslint-env browser*/
+/*eslint no-unused-vars: "error"*/
+
 document.querySelectorAll('.updateOuter').forEach(function(element, index) {
   var delay = index/18;
   element.setAttribute("style", "animation-delay: "+ delay +"s;");
 });
 function textFocus() {
-  document.querySelector('#textStatus').classList.add('lowered');
+  document.querySelectorAll('sInput').forEach(function(sInputs) {
+  sInputs.classList.add('lowered');
+  });
 }
