de2e8ea926388fb0f47e568bdc8e169a2100bac9
Jeremy Jacob <jeremjacob5705@student.lvusd.org>
2018-12-11 21:17:35 -0800

    Added hist. buttom

--- docs/js/attributes.js
+++ docs/js/attributes.js
@@ -1,10 +1,11 @@
 /*eslint-env browser*/
 /*eslint no-unused-vars: "error"*/
 
-document.querySelectorAll('.updateOuter').forEach(function(element, index) {
-  var delay = index/18;
-  element.setAttribute("style", "animation-delay: "+ delay +"s;");
-});
+//document.querySelectorAll('.updateOuter').forEach(function(element, index) {
+//  var delay = index/18;
+//  element.setAttribute("style", "animation-delay: "+ delay +"s;");
+//});
+
 function textFocus() {
   document.querySelectorAll('sInput').forEach(function(sInputs) {
   sInputs.classList.add('lowered');
